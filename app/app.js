ab54543d32ca65364c8ada4b5f9926f86321ae67
YOON-CC <whdbscks77@gmail.com>
2022-09-25 19:20:05 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const app = express();
 //앱 세팅
 app.set("views", "./src/views");  // ./views에 존재하는 js파일(프론트)을 찾을 것이다. 그러기 위해서는 먼저 get, render를 해야하는데...
 app.set("view engine", "ejs"); 
+app.use(express.static(`${__dirname}/src/public`))
 
 const home = require('./src/routes/home'); // get을 찾기위해 이동
 app.use("/", home); // 미들 웨어를 등록해주는 메서드   // 결국 이것을 통해서 routes/home까지 들어왔고 이후 js파일중 index.js에 먼저 접근이 된다. ㅋ
